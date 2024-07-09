dc90cfed9b82abe66f61a83628257bd8179e8e5a
pooji211 <dharak279@gmail.com>
2024-07-09 15:17:59 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //  treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3) //  code readability
+
+let name = "pooja"
+let age = 21
+let isLoggedIn = false
+
+//  number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true/false
+//  null =>  stanalone value
+//  undefined  =>  
+//  symbol => unique
+
+//  object
+
+console.log(typeof age);
